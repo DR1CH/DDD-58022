cec5b151b891bc8beedcc4a6088af4b844e519a5
EldrichMercado <98940968+EldrichMercado@users.noreply.github.com>
2023-05-11 12:30:23 +0800

    Add files via upload

---
+++ student.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE STUDENT ;
+
+SELECT *From student.students_INFO;
+SELECT * FROM student.students_info;
+SELECT * FROM student.students_info WHERE city = 'Laguna';
+SELECT * FROM student.students_info WHERE sem_grade = '80';
+SELECT * FROM student.students_info WHERE gender = 'F';
+SELECT * FROM student.students_info WHERE entry_age = '17';
+SELECT * FROM student.students_info WHERE final_exam = '100'; 
+SELECT * FROM student.students_info WHERE NOT status = '4TH YEAR';
+SELECT * FROM student.students_info ORDER BY sem_grade DESC;
+SELECT * FROM student.students_info WHERE gender = 'M' AND status = '1ST YEAR' ;
